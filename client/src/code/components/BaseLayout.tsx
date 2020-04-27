fa2d5f07ef831e5c757f5c08b6e6d4cc5fe55327
drew <dfleming@trackabout.com>
2020-04-27 10:31:30 -0400

    Working

--- client/src/code/components/BaseLayout.tsx
+++ client/src/code/components/BaseLayout.tsx
@@ -1,24 +1,46 @@
-import React from "react";
+// React imports.
+import React, { Component } from "react";
 
+// Library imports.
+
+// App imports.
 import ControlArea from "./ControlArea";
 import PlayerBoard from "./PlayerBoard";
-import {createState} from "../state";
+import { createState } from "../state";
 
-class BaseLayout extends React.Component {
-    state = createState(this);
 
-    render(){
-        return (
-            <div className="appContainer">
-                <div className="playerBoard">
-                    <PlayerBoard state={this.state} />
-                </div>
-                <div className="controlArea">
-                    <ControlArea state={this.state} />
-                </div>
-            </div>
-        );
-    }
-}
+/**
+ * The parent component of the entire app.
+ */
+class BaseLayout extends Component {
+
+
+  /**
+   * State data for the app.  This also includes all mutator functions for manipulating state.  That way, we only
+   * ever have to pass this entire object down through props (not necessarily the best design in terms of data
+   * encapsulation, but it does have the benefit of being quite a bit simpler).
+   */
+  state = createState(this);
+
+
+  /**
+   * Render().
+   */
+  render() {
+
+    return (
+
+      <div className="appContainer">
+        <div className="playerBoard"><PlayerBoard state={ this.state } /></div>
+        <div className="controlArea"><ControlArea state={ this.state } /></div>
+      </div>
+
+   );
+
+  } /* End render(). */
+
+
+} /* End class. */
+
 
 export default BaseLayout;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,77 +1,98 @@
+// Node imports.
 import path from "path";
-import express, { Express, json } from "express";
+
+// Library imports.
+import express, { Express } from "express";
 import WebSocket from "ws";
 
-type Player = {
-    score: number;
-    stillPlaying: boolean;
-}
 
-type GameState = {
-    players: {
-        [key: string]: Player
-    };
-}
+// Our collection players.  Each element is an object: { pid, score, stillPlaying }
+const players: any = { };
 
+
+// Construct Express server for client resources.
 const app: Express = express();
 app.use("/", express.static(path.join(__dirname, "../../client/dist")));
 app.listen(8001, () => {
-    console.log("BattleJong Express server ready.");
+  console.log("BattleJong Express server ready");
 });
 
-const wsServer = new WebSocket.Server({ port: 8080 }, function() {
-    console.log("BattleJong WebSocket server ready");
-});
 
+// Construct WebSocket server.
+const wsServer = new WebSocket.Server({ port : 8080 }, function() {
+  console.log("BattleJong WebSocket server ready");
+});
 wsServer.on("connection", (socket: WebSocket) => {
-    let game: GameState = {players:{}};
 
-    socket.on("message", (inMsg: string) => {
-        const msgParts: string[] = inMsg.toString().split("_");
-        const message: string = msgParts[0];
-        const pid: string = msgParts[1];
-        switch(message) {
-            case "match":
-                game.players[pid].score += parseInt(msgParts[2]);
-                wsServer.clients.forEach(c => c.send(`update_${pid}_${game.players[pid].score}`));
-            break;
+  console.log("Player connected");
 
-            case "done": 
-            game.players[pid].stillPlaying = false;
-                let playersDone: number = 0;
-                for(const player in game.players){
-                    if(game.players.hasOwnProperty(player)){
-                        if(!game.players[player].stillPlaying){
-                            playersDone++;
-                        }
-                    }
-                }
+  // First things first: hook up message handler.
+  socket.on("message", (inMsg: string) => {
 
-                if(playersDone === 2){
-                    let winningPID: string;
-                    const pids: string[] = Object.keys(game.players);
-                    if(game.players[pids[0]].score > game.players[pids[1]].score) {
-                        winningPID = pids[0];
-                    }
-                    else {
-                        winningPID = pids[1];
-                    }
+    console.log(`Message: ${inMsg}`);
 
-                    wsServer.clients.forEach(c => c.send(`gameOver_${winningPID}`));
-                }
+    const msgParts: string[] = inMsg.toString().split("_");
+    const message: string = msgParts[0];
+    const pid: string = msgParts[1];
+    switch (message) {
 
-            break;
+      // When a tile pair is matched: match_<pid>_<points>
+      case "match":
+        players[pid].score += parseInt(msgParts[2]);
+        // Broadcast score updates to both players.
+        wsServer.clients.forEach(function each(inClient: WebSocket) {
+          inClient.send(`update_${pid}_${players[pid].score}`);
+        });
+      break;
+
+      // When the player dead-ends or clears: done_<pid>
+      case "done":
+        players[pid].stillPlaying = false;
+        // See if both players are done playing.
+        let playersDone: number = 0;
+        for (const player in players) {
+          if (players.hasOwnProperty(player)) {
+            if (!players[player].stillPlaying) {
+              playersDone++;
+            }
+          }
         }
+        // They are both done playing, now see who won.
+        if (playersDone === 2) {
+          let winningPID: string;
+          const pids: string[] = Object.keys(players);
+          if (players[pids[0]].score > players[pids[1]].score) {
+            winningPID = pids[0];
+          } else {
+            winningPID = pids[1];
+          }
+          // Broadcast the outcome to both players.
+          wsServer.clients.forEach(function each(inClient: WebSocket) {
+            inClient.send(`gameOver_${winningPID}`);
+          });
+        }
+      break;
+
+    } /* End switch. */
+
+  }); /* End message handler. */
+
+  // Now, construct PID for this player and add the player to the collection.
+  const pid: string = `pid${new Date().getTime()}`;
+  players[pid] = { score : 0, stillPlaying : true };
+
+  // Inform the player that we're connected and give them their ID.
+  socket.send(`connected_${pid}`);
+
+  // If there are now two players, transition state on the clients.  This broadcasts to ALL clients, even the one
+  // that sent the current message, which is what we want in this case.
+  if (Object.keys(players).length === 2) {
+    // Shuffle the tiles in the layout so we can send the layout to both clients.
+    const shuffledLayout: number[][][] = shuffle();
+    wsServer.clients.forEach(function each(inClient: WebSocket) {
+      inClient.send(`start_${JSON.stringify(shuffledLayout)}`);
     });
-
-    const pid: string = `pid${new Date().getTime()}`;
-    game.players[pid] = {score: 0, stillPlaying: true};
-    socket.send(`connected_${pid}`);
-
-    if(Object.keys(game.players).length === 2) {
-        const shuffledLayout: number[][][] = shuffle();
-        wsServer.clients.forEach(c => c.send(`start_${JSON.stringify(shuffledLayout)}`));
-    }
+  }
 });
 
 // ---------------------------------------- Game code. ----------------------------------------
@@ -81,108 +102,108 @@ wsServer.on("connection", (socket: WebSocket) => {
 // When board is shuffled, all 1's become 101-142 (matching the 42 tile type filenames).
 // Tile 101 is wildcard.
 const layout: number[][][] = [
-    /* Layer 1. */
-    [
-      [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
-      [ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 ],
-      [ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
-      [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
-      [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-      [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
-      [ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
-      [ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 ],
-      [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
-    ],
-    /* Layer 2. */
-    [
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    ],
-    /* Layer 3. */
-    [
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    ],
-    /* Layer 4. */
-    [
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    ],
-    /* Layer 5. */
-    [
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-      [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    ]
-  ]; /* End layout. */
-  
-  
-  /**
-   * Shuffles the tiles in the layout, randomizing tile placement.  Note that this uses the "American-style
-   * totally random approach, which means that not every shuffle will be "winnable" (meaning that there may be no
-   * path to completely clear the board).
-   *
-   * @return A shuffled layout.
-   */
-  function shuffle(): number[][][] {
-  
-    // Clone the layout.
-    const cl: number[][][] = layout.slice(0);
-  
-    // We need to ensure no more than 4 wildcards are placed, so this will count them.
-    let numWildcards: number = 0;
-  
-    // Iterate over the entire board, randomly choosing a tile for each position that isn't supposed to be blank.
-    const numTileTypes: number = 42;
-    for (let l: number = 0; l < cl.length; l++) {
-      const layer: number[][] = cl[l];
-      for (let r: number = 0; r < layer.length; r++) {
-        const row: number[] = layer[r];
-        for (let c: number = 0; c < row.length; c++) {
-          const tileVal: number = row[c];
-          // tileVal > 0 indicates there is supposed to be a tile at this position.
-          if (tileVal === 1) {
-            row[c] = (Math.floor(Math.random() * numTileTypes)) + 101;
-            // If this is a wildcard and no more are allowed then bump to the next tile type, otherwise bump
-            // wildcard count.  Doing this is a cheap way of having to randomly select a tile again, which at this
-            // point could actually be a little tricky if we want to avoid duplicate code.
-            if (row[c] === 101 && numWildcards === 3) {
-              row[c] = 102;
-            } else {
-              numWildcards += numWildcards;
-            }
-          } /* End tileVal > 0 check. */
-        } /* End column iteration. */
-      } /* End row iteration. */
-    } /* End layer iteration. */
-  
-    return cl;
-  
-  } /* End shuffle(). */
+  /* Layer 1. */
+  [
+    [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
+    [ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 ],
+    [ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
+    [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
+    [ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
+    [ 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 ],
+    [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
+  ],
+  /* Layer 2. */
+  [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  ],
+  /* Layer 3. */
+  [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  ],
+  /* Layer 4. */
+  [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  ],
+  /* Layer 5. */
+  [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  ]
+]; /* End layout. */
+
+
+/**
+ * Shuffles the tiles in the layout, randomizing tile placement.  Note that this uses the "American-style
+ * totally random approach, which means that not every shuffle will be "winnable" (meaning that there may be no
+ * path to completely clear the board).
+ *
+ * @return A shuffled layout.
+ */
+function shuffle(): number[][][] {
+
+  // Clone the layout.
+  const cl: number[][][] = layout.slice(0);
+
+  // We need to ensure no more than 4 wildcards are placed, so this will count them.
+  let numWildcards: number = 0;
+
+  // Iterate over the entire board, randomly choosing a tile for each position that isn't supposed to be blank.
+  const numTileTypes: number = 42;
+  for (let l: number = 0; l < cl.length; l++) {
+    const layer: number[][] = cl[l];
+    for (let r: number = 0; r < layer.length; r++) {
+      const row: number[] = layer[r];
+      for (let c: number = 0; c < row.length; c++) {
+        const tileVal: number = row[c];
+        // tileVal > 0 indicates there is supposed to be a tile at this position.
+        if (tileVal === 1) {
+          row[c] = (Math.floor(Math.random() * numTileTypes)) + 101;
+          // If this is a wildcard and no more are allowed then bump to the next tile type, otherwise bump
+          // wildcard count.  Doing this is a cheap way of having to randomly select a tile again, which at this
+          // point could actually be a little tricky if we want to avoid duplicate code.
+          if (row[c] === 101 && numWildcards === 3) {
+            row[c] = 102;
+          } else {
+            numWildcards += numWildcards;
+          }
+        } /* End tileVal > 0 check. */
+      } /* End column iteration. */
+    } /* End row iteration. */
+  } /* End layer iteration. */
+
+  return cl;
+
+} /* End shuffle(). */
